9817ec21c66efb9098768e82e6057c7d2aa269e7
Frey <freyandhy@gmail.com>
2019-05-18 01:42:41 +0700

    Updates

---
+++ precache-manifest.d223fd2639f6bddfef30f1f120353ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e710f04bf7c0327c94b49f4ffbf880a",
+    "url": "/react-news/index.html"
+  },
+  {
+    "revision": "6edfa2f73e307970b25e",
+    "url": "/react-news/static/css/main.715616db.chunk.css"
+  },
+  {
+    "revision": "06ccaf4e6c8803beaee5",
+    "url": "/react-news/static/js/2.0468f5f2.chunk.js"
+  },
+  {
+    "revision": "6edfa2f73e307970b25e",
+    "url": "/react-news/static/js/main.f5f9afbb.chunk.js"
+  },
+  {
+    "revision": "a13361ecc00b0ba12a21",
+    "url": "/react-news/static/js/runtime~main.2971d4cb.js"
+  },
+  {
+    "revision": "41c8cc4aa574674e3775be16490836dd",
+    "url": "/react-news/static/media/loading.41c8cc4a.svg"
+  }
+]);
